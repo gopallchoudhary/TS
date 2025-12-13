272804f43a97602a9e1f8b5aafd7346ae0a6d0e6
gopallchoudhary <gopalchoudhary521@gmail.com>
2025-12-13 22:37:43 +0530

    functions

---
+++ src/functions.ts
@@ -0,0 +1,15 @@
+function makeOrder(order: string): string | null {
+    if (!order) return null
+    return order
+}
+
+
+function createTea(order: {
+    type: string,
+    sugar: number,
+    size: "small" | "large"
+}): {} {
+    return order
+}
+
+
